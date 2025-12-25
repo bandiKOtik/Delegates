eb5162f88eb4f3edb773fcbd45af073853bd90c5
bandiKOtik <muxinde2003@gmail.com>
2025-12-25 20:12:29 +0300

    HOTFIX v0.00000001

--- Assets/Scripts/Wallet/Wallet.cs
+++ Assets/Scripts/Wallet/Wallet.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using UnityEngine;
 
 public enum Currencies
 {
@@ -13,26 +12,22 @@ public class Wallet
 {
     public event Action<Currencies, int> OnCurrencieChanged;
 
-    public int Coins { get; private set; }
-    public int Gems { get; private set; }
-    public int Energy { get; private set; }
-
-    public readonly Dictionary<Currencies, int> _currencyStash;
+    public readonly Dictionary<Currencies, int> CurrencyStash;
 
     public Wallet()
     {
-        _currencyStash = new Dictionary<Currencies, int>
+        CurrencyStash = new Dictionary<Currencies, int>
         {
-            { Currencies.Coins, Coins},
-            { Currencies.Gems, Gems},
-            { Currencies.Energy, Energy}
+            { Currencies.Coins, default},
+            { Currencies.Gems, default},
+            { Currencies.Energy, default}
         };
     }
 
     public void ChangeCurrencieValue(Currencies currencie, int amount)
     {
-        _currencyStash[currencie] += amount;
+        CurrencyStash[currencie] += amount;
 
-        OnCurrencieChanged?.Invoke(currencie, _currencyStash[currencie]);
+        OnCurrencieChanged?.Invoke(currencie, CurrencyStash[currencie]);
     }
 }
